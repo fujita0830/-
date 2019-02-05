74291f2dec6192509747676f0bde2e192bb344f5
nk19930907 <tatitute123456789@gmail.com>
2019-02-05 17:42:08 +0900

    ResetPasswordAction ResetPasswordConfirmAction RestPasswordCompletaAction
    おわた

---
+++ src/com/internousdev/cyan/action/ResetPasswordAction.java
@@ -0,0 +1,38 @@
+package com.internousdev.cyan.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ResetPasswordAction extends ActionSupport implements SessionAware{
+	private String categoryId;
+	private Map<String,Object> session;
+	public String execute() {
+		String result = ERROR;
+		session.remove("loginidErrorMessageList");
+		session.remove("passwordErrorMessageList");
+		session.remove("passwordIncorrectErrorMessageList");
+		session.remove("newPasswordErrorMessageList");
+		session.remove("reConfirmateionNewPasswordErrorMessageList");
+		session.remove("newPasswordIncorrectErrorMessageList");
+
+		result = SUCCESS;
+		return result;
+	}
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public Map<String, Object> getSession() {
+		return session;
+	}
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+
+}
