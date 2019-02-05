712c908a518aa0f7b27384788b90550fab343b0a
fujita0830 <fntd.0830@gmail.com>
2019-02-05 14:54:00 +0900

    MCategoryDTOとMCategoryDAO完成

---
+++ src/com/internousdev/cyan/dto/MCategoryDTO.java
@@ -0,0 +1,61 @@
+package com.internousdev.cyan.dto;
+
+public class MCategoryDTO {
+
+	private String id;
+	private String categoryId;
+	private String categoryName;
+	private String description;
+	private String insertDate;
+	private String updateDate;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getInsertDate() {
+		return insertDate;
+	}
+
+	public void setInsertDate(String insertDate) {
+		this.insertDate = insertDate;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+
+}
