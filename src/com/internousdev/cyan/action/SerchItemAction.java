339a87f305ad1e9ffe27b22f2f8b689a45af119e
fujita0830 <fntd.0830@gmail.com>
2019-02-06 14:35:44 +0900

    作成完了　藤田

--- src/com/internousdev/cyan/action/SerchItemAction.java
+++ src/com/internousdev/cyan/action/SerchItemAction.java
@@ -1,11 +1,14 @@
 package com.internousdev.cyan.action;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.internousdev.cyan.dao.MCategoryDAO;
+import com.internousdev.cyan.dao.ProductInfoDAO;
 import com.internousdev.cyan.dto.MCategoryDTO;
 import com.internousdev.cyan.dto.ProductInfoDTO;
 import com.internousdev.cyan.util.InputChecker;
@@ -23,31 +26,104 @@ public class SerchItemAction extends ActionSupport {
 
 	public String execute(){
 
-		String reslut = ERROR;
+		String result = ERROR;
 		InputChecker inputChecker = new InputChecker();
 
 		if (StringUtils.isBlank(keywords)){
-
 			tempKeywords = "";
-
-
 		}else{
-			keywordsErrorMessageList = inputChecker.doCheck("検索ワード", keywords, 0, 16, true, true, true, true, false, false, false, true, true);
-
-
+			tempKeywords = keywords.replaceAll("　"," ").replaceAll("\\s{2,}"," ");
 		}
 
+		if(!(tempKeywords.equals(""))){
+			keywordsErrorMessageList = inputChecker.doCheck("検索ワード", keywords, 0, 16, true, true, true, true, false, false, false, true, true);
+			Iterator<String> iterator = keywordsErrorMessageList.iterator();
+			if(iterator.hasNext()) {
+				session.put("keywordsErrorMessageList", keywordsErrorMessageList);
+				return SUCCESS;
+			}
+		}
 
+		ProductInfoDAO productInfoDAO = new ProductInfoDAO();
+		switch (categoryId) {
+			case "1":
+				productInfoDtoList = productInfoDAO.getProductInfoListAll(tempKeywords.split(" "));
+				result = SUCCESS;
+				break;
 
+			default:
+				productInfoDtoList = productInfoDAO.getProductInfoListByKeywords(tempKeywords.split(" "), categoryId);
+				result = SUCCESS;
+				break;
+		}
 
+		Iterator<ProductInfoDTO> iterator = productInfoDtoList.iterator();
+		if(!(iterator.hasNext())) {
+			productInfoDtoList = null;
+		}
 
-
-
-
-		return SUCCESS;
-
+		if(!session.containsKey("mCategoryList")) {
+			MCategoryDAO mCategoryDao = new MCategoryDAO();
+			mCategoryDTOList = mCategoryDao.getMCategoryList();
+			session.put("mCategoryDTOList", mCategoryDTOList);
+		}
+		return result;
 	}
 
+	public String getCategoryId() {
+		return categoryId;
+	}
 
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public List<MCategoryDTO> getmCategoryDTOList() {
+		return mCategoryDTOList;
+	}
+
+	public void setmCategoryDTOList(List<MCategoryDTO> mCategoryDTOList) {
+		this.mCategoryDTOList = mCategoryDTOList;
+	}
+
+	public List<String> getKeywordsErrorMessageList() {
+		return keywordsErrorMessageList;
+	}
+
+	public void setKeywordsErrorMessageList(List<String> keywordsErrorMessageList) {
+		this.keywordsErrorMessageList = keywordsErrorMessageList;
+	}
+
+	public List<ProductInfoDTO> getProductInfoDtoList() {
+		return productInfoDtoList;
+	}
+
+	public void setProductInfoDtoList(List<ProductInfoDTO> productInfoDtoList) {
+		this.productInfoDtoList = productInfoDtoList;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	public String getTempKeywords() {
+		return tempKeywords;
+	}
+
+	public void setTempKeywords(String tempKeywords) {
+		this.tempKeywords = tempKeywords;
+	}
 
 }
