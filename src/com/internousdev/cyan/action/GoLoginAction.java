1d51905ffa157ef72dc1c95069dbd8066dc62b31
kf-haru <haru57.m@gmail.com>
2019-02-12 13:01:53 +0900

    #30 LoginAction.javaの修正完了　けいこ

--- src/com/internousdev/cyan/action/GoLoginAction.java
+++ src/com/internousdev/cyan/action/GoLoginAction.java
@@ -1,17 +1,13 @@
 package com.internousdev.cyan.action;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
-import com.internousdev.cyan.dto.MCategoryDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class GoLoginAction extends ActionSupport implements SessionAware {
 	private String categoryId;
-	private List<MCategoryDTO> mCategoryDTOList = new ArrayList<MCategoryDTO>();
 	private Map<String, Object> session;
 	public String execute() {
 		String result = SUCCESS;
@@ -29,14 +25,6 @@ public class GoLoginAction extends ActionSupport implements SessionAware {
 
 	public void setCategoryId(String categoryId) {
 		this.categoryId = categoryId;
-	}
-
-	public List<MCategoryDTO> getmCategoryDTOList() {
-		return mCategoryDTOList;
-	}
-
-	public void setmCategoryDTOList(List<MCategoryDTO> mCategoryDTOList) {
-		this.mCategoryDTOList = mCategoryDTOList;
 	}
 
 	public Map<String, Object> getSession() {
